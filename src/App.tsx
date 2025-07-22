eeab1cb4a2df2b1550b915287283191af3d34089
Narendra Kumar Choudhary <narendrakumarrank20@gmail.com>
2025-07-23 03:45:19 +0530

    change heading text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AuroraText } from "./components/lightswind/aurora-text-effect";
 
 const App = () => {
-  return <AuroraText text="I love You Teju" />;
+  return <AuroraText text="Sorry Teju I love you" />;
 };
 
 export default App;
